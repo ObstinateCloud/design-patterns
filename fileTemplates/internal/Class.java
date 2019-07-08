3610f0e4a86a84473840019779fc02fc00dce35b
zjyWorkPc <zjyWorkPc@git.com>
2019-07-08 22:41:55 +0800

    IU-2017.2.6 <Administrator@EDZ-20170707SWL Create ide.general.xml, git.xml, print.xml, gitosc_settings.xml, project.default.xml, diff.xml, other.xml, Default.xml, find.xml, ui.lnf.xml, lombok.xml, filetypes.xml, projectView.xml, webDeployTransportHostTargets.xml, plugin_ui.xml, notifications.xml, VisualVMLauncher.xml, databaseDrivers.xml, github_settings.xml, javarebel.xml, Default.xml, laf.xml, Class.java, jdk.table.xml, debugger.renderers.xml, path.macros.xml, Eclipse copy.xml, vcs.xml, editor.codeinsight.xml, databaseSettings.xml, toStringTemplates.xml, editor.xml, debugger.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/*
+*@auth zxw
+*@date ${sysdate}
+*@description:
+*/
+public class ${NAME} {
+}
